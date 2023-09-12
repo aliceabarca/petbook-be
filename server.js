6a6a88ac1eb94602b89368e163ff5a8026700455
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-09-12 01:36:51 -0400

    Add type to locals and post request.

--- server.js
+++ server.js
@@ -8,18 +8,43 @@ app.use(cors());
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Pet Book';
 
-// app.locals.pets = [
-  // { id: '1', petOwnersName: 'Judy', petName: 'Oreo', type: 'cat', breed: 'domesticShorthair'},
-  // { id: '2', petOwnersName: 'Alex', petName: 'Duke', type: 'dog', breed: 'schnoodle'},
-  // { id: '3', petOwnersName: 'Paulina', petName: 'Pluto', type: 'cat', breed: 'Bombay'},
-  // { id: '4', petOwnersName: 'Alice', petName: 'Rocco', type: 'dog', breed:'pitbull'},
-// ];
-
 app.locals.pets = [
-{id: '1', petName: 'Oreo', petNickname: 'Oreo', petAge: '2.5', petFunFact: 'hes had 2 types of tape worms', petOwnersName: 'Judy'},
-{id: '2', petName: 'Duke', petNickname: 'Doodle-bee', petAge: '19', petFunFact: 'He likes to go for car rides', petOwnersName: 'Alex'},
-{id: '3', petName: 'Pluto', petNickname: 'Pluto', petAge: '1', petFunFact: 'hes really fun', petOwnersName: 'Paulina'},
-{id: '4', petName: 'Rocco', petNickname: 'Rocco', petAge: '1', petFunFact: 'hes really really fun', petOwnersName: 'Alice'}
+  {
+    id: '1', 
+    petName: 'Oreo', 
+    petNickname: 'Oreo', 
+    petAge: '2.5', 
+    petFunFact: 'hes had 2 types of tape worms', 
+    petOwnersName: 'Judy',
+    type: 'Cat'
+  },
+  {
+    id: '2', 
+    petName: 'Duke', 
+    petNickname: 'Doodle-bee', 
+    petAge: '19', 
+    petFunFact: 'He likes to go for car rides', 
+    petOwnersName: 'Alex',
+    type: 'Dog'
+  },
+  {
+    id: '3',
+    petName: 'Pluto', 
+    petNickname: 'Pluto', 
+    petAge: '1', 
+    petFunFact: 'hes really fun', 
+    petOwnersName: 'Paulina',
+    type: 'Cat'
+  },
+  {
+    id: '4', 
+    petName: 'Rocco', 
+    petNickname: 'Rocco', 
+    petAge: '1', 
+    petFunFact: 'hes really really fun', 
+    petOwnersName: 'Alice',
+    type: 'Dog'
+  }
 ];
 
 
@@ -56,9 +81,9 @@ app.post('/api/v1/pets', (request, response) => {
     }
   }
 
-  const { petName, petNickname, petAge, petFunFact, petOwnersName} = pet;
-  app.locals.pets.push({ id, petName, petNickname, petAge, petFunFact, petOwnersName});
-  response.status(201).json({ id, petName, petNickname, petAge, petFunFact, petOwnersName});
+  const { petName, petNickname, petAge, petFunFact, petOwnersName, type } = pet;
+  app.locals.pets.push({ id, petName, petNickname, petAge, petFunFact, petOwnersName, type});
+  response.status(201).json({ id, petName, petNickname, petAge, petFunFact, petOwnersName, type});
 });
 
 app.listen(app.get('port'), () => {
