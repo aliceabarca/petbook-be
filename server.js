ce10b6fd198d7bcaf43b7b4555f46f38ee5ab7e2
Judy Ye <judyy.ye@gmail.com>
2023-09-13 00:31:08 -0700

    Add data to app.locals.pet

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.locals.pets = [
     id: 1, 
     petName: 'Oreo', 
     petNickname: 'Oreo', 
-    petAge: '2.5', 
+    petAge: 2.5, 
     petFunFact: 'hes had 2 types of tape worms', 
     petOwnersName: 'Judy',
     type: 'Cat'
@@ -22,7 +22,7 @@ app.locals.pets = [
     id: 2, 
     petName: 'Duke', 
     petNickname: 'Doodle-bee', 
-    petAge: '19', 
+    petAge: 19, 
     petFunFact: 'He likes to go for car rides', 
     petOwnersName: 'Alex',
     type: 'Dog'
@@ -31,7 +31,7 @@ app.locals.pets = [
     id: 3,
     petName: 'Pluto', 
     petNickname: 'Pluto', 
-    petAge: '1', 
+    petAge: 1, 
     petFunFact: 'hes really fun', 
     petOwnersName: 'Paulina',
     type: 'Cat'
@@ -40,11 +40,92 @@ app.locals.pets = [
     id: 4, 
     petName: 'Rocco', 
     petNickname: 'Rocco', 
-    petAge: '1', 
+    petAge: 1, 
     petFunFact: 'hes really really fun', 
     petOwnersName: 'Alice',
     type: 'Dog'
-  }
+  },
+  {
+    id: 5, 
+    petName: 'Dusty', 
+    petNickname: 'Dustin Dustwald Dusty Weasel Wensloff', 
+    petAge: 5, 
+    petFunFact: 'He loves to sploot in a creek', 
+    petOwnersName: 'Jocelyn Wensloff',
+    type: 'Dog'
+  },
+  {
+    id: 6, 
+    petName: 'Ivan', 
+    petNickname: 'Bubba/Baby', 
+    petAge: 10, 
+    petFunFact: 'He loves toilet paper!', 
+    petOwnersName: 'Banjamin Hatch',
+    type: 'Dog'
+  },
+  {
+    id: 7, 
+    petName: 'Sentinel', 
+    petNickname: 'Sen', 
+    petAge: 5, 
+    petFunFact: 'Her preferred toy is mesh potato bags', 
+    petOwnersName: 'Renee Pinna',
+    type: 'Cat'
+  },
+  {
+    id: 8, 
+    petName: 'Khaleesi', 
+    petNickname: 'Leesi/Khalees', 
+    petAge: 7, 
+    petFunFact: 'She doesn\'t like citrus fruit but will eat anything else', 
+    petOwnersName: 'Josh Bennett',
+    type: 'Dog'
+  },
+  {
+    id: 9, 
+    petName: 'Clyde', 
+    petNickname: 'Clyborg, the glide, Clyderman, Clydawg, etc.', 
+    petAge: 1.5, 
+    petFunFact: 'He\'s the runt of his litter!', 
+    petOwnersName: 'Avery Berryman',
+    type: 'Dog'
+  },
+  {
+    id: 10, 
+    petName: 'Tir', 
+    petNickname: '', 
+    petAge: null, 
+    petFunFact: '', 
+    petOwnersName: 'Parvin Sattorova',
+    type: 'Dog'
+  },
+  {
+    id: 11, 
+    petName: 'Echo', 
+    petNickname: 'echo whecko', 
+    petAge: 3, 
+    petFunFact: 'Echo loves to play outside with the chickens', 
+    petOwnersName: 'Scotty Brown',
+    type: 'Dog'
+  },
+  {
+    id: 12, 
+    petName: 'Rocky', 
+    petNickname: 'Rockaroo', 
+    petAge: 13, 
+    petFunFact: 'he loves to sleep under the deck during the day', 
+    petOwnersName: 'Jen Ziel',
+    type: 'Dog'
+  },
+  {
+    id: 13, 
+    petName: 'Bailey', 
+    petNickname: 'bear boyyyy/sweet man/honey boy', 
+    petAge: 10, 
+    petFunFact: 'bailey has two metal knees', 
+    petOwnersName: 'Lauren DeLaRosa',
+    type: 'Dog'
+  },
 ];
 
 
